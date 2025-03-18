251b40e32e5d175d74944da07aaf8da3127f26a4
Felipe Ferrari <felferrari@gmail.com>
2025-03-18 19:40:45 -0300

    up

--- src/data/data_module.py
+++ src/data/data_module.py
@@ -1,1 +1,38 @@
+from lightning import LightningDataModule
+from torch.utils.data import Dataset
+from lightning.pytorch.utilities.types import TRAIN_DATALOADERS
 
+class DataModule(LightningDataModule):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        
+    def train_dataloader(self) -> TRAIN_DATALOADERS:
+        pass
+    
+    def val_dataloader(self) -> TRAIN_DATALOADERS:
+        pass
+        
+    
+class TrainDataset(Dataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, index):
+        pass
+        
+        
+        
+class PredDataset(Dataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def __len__(self):
+        pass
+    
+    def __getitem__(self, index):
+        pass
